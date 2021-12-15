fc84862bf18e644c468a0c0a43bdda2e26c69a91
kvammee <elliott.kvamme@gmail.com>
2021-12-15 16:46:57 -0600

    Updated index files to add routing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
+const apiRoutes = require('./api/index.js');
 
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
